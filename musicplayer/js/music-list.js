8f3a6d6385c9b93df20d4700905da98f870c9682
jliliflora <61509359+jliliflora@users.noreply.github.com>
2022-04-21 20:03:04 +0900

    JavaScript Project

--- musicplayer/js/music-list.js
+++ musicplayer/js/music-list.js
@@ -11,37 +11,37 @@
 
 let allMusic = [
   {
-    name: "Harley Bird - Home",
-    artist: "Jordan Schor",
-    img: "music-1",
-    src: "music-1"
+    name: "Black Swan",
+    artist: "BTS",
+    img: "blackswan",
+    src: "BlackSwan"
   },
   {
-    name: "Ikson Anywhere – Ikson",
-    artist: "Audio Library",
-    img: "music-2",
-    src: "music-2"
+    name: "Save Me",
+    artist: "BTS",
+    img: "saveme",
+    src: "SaveMe"
   },
   {
-    name: "Beauz & Jvna - Crazy",
+    name: "Crazy",
     artist: "Beauz & Jvna",
     img: "music-3",
     src: "music-3"
   },
   {
-    name: "Hardwind - Want Me",
+    name: "Want Me",
     artist: "Mike Archangelo",
     img: "music-4",
     src: "music-4"
   },
   {
-    name: "Jim - Sun Goes Down",
+    name: "Sun Goes Down",
     artist: "Jim Yosef x Roy",
     img: "music-5",
     src: "music-5"
   },
   {
-    name: "Lost Sky - Vision NCS",
+    name: "Vision NCS",
     artist: "NCS Release",
     img: "music-6",
     src: "music-6"

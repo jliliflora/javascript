b55ee39cb8744a2d4e923465b92a5baa939acc60
jliliflora <61509359+jliliflora@users.noreply.github.com>
2022-05-14 06:34:14 +0900

    JavaScript Project

---
+++ musicplayer/js/music-list.js
@@ -0,0 +1,62 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "불타오르네",
+    artist: "BTS",
+    img: "불타오르네",
+    src: "불타오르네"
+  },
+  {
+    name: "Black Swan",
+    artist: "BTS",
+    img: "blackswan",
+    src: "BlackSwan"
+  },
+  {
+    name: "Save Me",
+    artist: "BTS",
+    img: "saveme",
+    src: "SaveMe"
+  },
+  {
+    name: "House of Cards",
+    artist: "BTS",
+    img: "houseofcards",
+    src: "HouseOfCards"
+  },
+  {
+    name: "쩔어",
+    artist: "BTS",
+    img: "쩔어",
+    src: "쩔어"
+  },
+  {
+    name: "Butterfly",
+    artist: "BTS",
+    img: "butterfly",
+    src: "Butterfly"
+  },
+  {
+    name: "Love Is Not Over",
+    artist: "BTS",
+    img: "loveisnotover",
+    src: "LoveIsNotOver"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
